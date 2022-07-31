6665bbfbf30d63102b552595ff9c582484131c8c
Decoderseeman <99487702+Decoderseeman@users.noreply.github.com>
2022-07-31 18:12:20 +0530

    Update txt.py

--- helper/txt.py
+++ helper/txt.py
@@ -25,56 +25,52 @@ License Link : https://github.com/TEAM-PYRO-BOTZ/PYRO-RENAME-BOT/blob/main/LICEN
 
 class mr(object):
     PROGRESS_BAR = """\n
-╭━━━━❰ᴘʀᴏɢʀᴇss ʙᴀʀ❱━➣
-┣⪼ 🗂️ : {1} | {2}
-┣⪼ ⏳️ : {0}%
-┣⪼ 🚀 : {3}/s
-┣⪼ ⏱️ : {4}
+╭━━━━❰<b> ᴘʀᴏɢʀᴇss ʙᴀʀ </b>❱━➣
+┣⪼ 🗂️ **:** {1} | {2}
+┣⪼ ⏳️ <b>:</b> {0}%
+┣⪼ 🚀 <b>:</b> {3}/s
+┣⪼ ⏱️ <b>:</b> {4}
 ╰━━━━━━━━━━━━━━━➣ """
 
     ABOUT_TXT = """
-╭───────────⍟
-├🤖 𝙼𝚈 𝙽𝙰𝙼𝙴 : {}
-├👑 𝙾𝚆𝙽𝙴𝚁 : <a href=https://t.me/PYRO_BOTZ>𝗧𝗘𝗔𝗠 𝗣𝗬𝗥𝗢 𝗕𝗢𝗧𝗭</a> 
-├👨‍💻 𝙿𝚁𝙾𝙶𝚁𝙰𝙼𝙴𝚁 : <a href=https://github.com/lntechnical2>𝙻𝙽 𝚃𝙴𝙲𝙷.𝚐𝚒𝚝</a>
-├📕 𝙻𝙸𝙱𝚁𝙰𝚁𝚈 : <a href=https://github.com/pyrogram>𝚈𝚁𝙾𝙶𝚁𝙰𝙼</a>
-├✏️ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴 : <a href=https://www.python.org>𝙿𝚈𝚃𝙷𝙾𝙽 3</a>
-├💾 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴 : <a href=https://cloud.mongodb.com>𝙼𝙾𝙽𝙶𝙾 𝙳𝙱</a>
-├🌀 𝙼𝚈 𝚂𝙴𝚁𝚅𝙴𝚁 : <a href=https://dashboard.heroku.com>𝙷𝙴𝚁𝙾𝙺𝚄</a>
-├📊 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚄𝚂 : v3.6.8 [ 𝙼𝙰𝙹𝙾𝚁 ]              
+╭━━━━❰<b> ᴀʙᴏᴜᴛ ᴍᴇ </b>❱━➣
+├🤖 𝙼𝚈 𝙽𝙰𝙼𝙴 <b>:</b> {}
+├👑 𝙾𝚆𝙽𝙴𝚁 <b>:</b> <a href=https://t.me/ajay_king_x>♚ ᴀᴊᴀʏ ♚</a> 
+├👨‍💻 𝙿𝚁𝙾𝙶𝚁𝙰𝙼𝙴𝚁 <b>:</b> <a href=https://t.me/mkv_bots>𝗠𝗞𝗩 𝗕𝗢𝗧𝗦</a>
+├📕 𝙻𝙸𝙱𝚁𝙰𝚁𝚈 <b>:</b> <a href=https://github.com/pyrogram>𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼</a>
+├✏️ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴 <b>:</b> <a href=https://www.python.org>𝙿𝚈𝚃𝙷𝙾𝙽 3</a>
+├💾 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴 <b>:</b> <a href=https://cloud.mongodb.com>𝙼𝙾𝙽𝙶𝙾 𝙳𝙱</a>
+├🌀 𝙼𝚈 𝚂𝙴𝚁𝚅𝙴𝚁 <b>:</b> <a href=https://dashboard.heroku.com>𝙷𝙴𝚁𝙾𝙺𝚄</a>
+├📊 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚄𝚂 <b>:</b> v3.6.8 [ 𝙼𝙰𝙹𝙾𝚁 ]              
 ╰───────────────⍟
                                 """
     HELP_TXT = """
-🌌 <b><u>HOW TO SET THUMBNILE</u></b>
+🌌 <b>𝙷𝙾𝚆 𝚃𝙾 𝚂𝙴𝚃 𝚃𝙷𝚄𝙼𝙱𝙽𝙰𝙸𝙻</b>
   
-•> /start a bot and send any picture to automatically set thumbnile.
-•> /delthumb use this command and delete your old thumbnile.
-•> /viewthumb use this command view your current thumbnile.
+•> <b>/start 𝙰 𝙱𝙾𝚃 𝙰𝙽𝙳 𝚂𝙴𝙽𝙳 𝙰𝙽𝚈 𝙿𝙸𝙲𝚃𝚄𝚁𝙴 𝚃𝙾 𝙰𝚄𝚃𝙾𝙼𝙰𝚃𝙸𝙲𝙰𝙻𝙻𝚈 𝚂𝙴𝚃 𝚃𝙷𝚄𝙼𝙱𝙽𝙰𝙸𝙻.</b>
+•> <b>/delthumb 𝚄𝚂𝙴 𝚃𝙷𝙸𝚂 𝙲𝙾𝙼𝙼𝙰𝙽𝙳 𝙰𝙽𝙳 𝙳𝙴𝙻𝙴𝚃𝙴 𝚈𝙾𝚄𝚁 𝙾𝙻𝙳 𝚃𝙷𝚄𝙼𝙱𝙽𝙰𝙸𝙻.</b>
+•> <b>/viewthumb 𝚄𝚂𝙴 𝚃𝙷𝙸𝚂 𝙲𝙾𝙼𝙼𝙰𝙽𝙳 𝚅𝙸𝙴𝚆 𝚈𝙾𝚄𝚁 𝙲𝚄𝚁𝚁𝙴𝙽𝚃 𝚃𝙷𝚄𝙼𝙱𝙽𝙰𝙸𝙻. </b>
 
-📑 <b><u>HOW TO SET CUSTOM CAPTION</u></b>
-•> /set_caption - set a custom caption
-•> /see_caption - see your custom caption
-•> /del_caption - delete custom caption
+📑 <b>𝙷𝙾𝚆 𝚃𝙾 𝚂𝙴𝚃 𝙲𝚄𝚂𝚃𝙾𝙼 𝙲𝙰𝙿𝚃𝙸𝙾𝙽</b>
+•> <b>/set_caption - 𝚂𝙴𝚃 𝙰 𝙲𝚄𝚂𝚃𝙾𝙼 𝙲𝙰𝙿𝚃𝙸𝙾𝙽</b>
+•> <b>/see_caption - 𝚂𝙴𝙴 𝚈𝙾𝚄𝚁 𝙲𝚄𝚂𝚃𝙾𝙼 𝙲𝙰𝙿𝚃𝙸𝙾𝙽</b>
+•> <b>/del_caption - 𝙳𝙴𝙻𝙴𝚃𝙴 𝙲𝚄𝚂𝚃𝙾𝙼 𝙲𝙰𝙿𝚃𝙸𝙾𝙽</b>
 
-Example:- /set_caption 📕 File Name: {filename}
-💾 Size: {filesize}
-⏰ Duration: {duration}
+<b>𝙴𝚇𝙰𝙼𝙿𝙻𝙴</b> :- /set_caption 📕 <b>𝙵𝙸𝙻𝙴 𝙽𝙰𝙼𝙴 </b>: {filename}
+💾 <b>𝙵𝙸𝙻𝙴 𝚂𝙸𝚉𝙴 </b>: {filesize}
+⏰ <b>𝙳𝚄𝚁𝙰𝚃𝙸𝙾𝙽 </b>: {duration}
 
-✏️ <b><u>HOW TO RENAME A FILE</u></b>
-•> send any file and click rename option and type new file name and \n send select [ document, video, audio ]👈 choice this.
-ℹ️ 𝗔𝗻𝘆 𝗢𝘁𝗵𝗲𝗿 𝗛𝗲𝗹𝗽 𝗖𝗼𝗻𝘁𝗮𝗰𝘁 :- <a href=https://t.me/PYRO_BOTZ_CHAT>𝑺𝑼𝑷𝑷𝑶𝑹𝑻 𝑮𝑹𝑶𝑼𝑷</a>
+✏️ <b>𝙷𝙾𝚆 𝚃𝙾 𝚁𝙴𝙽𝙰𝙼𝙴 𝙰 𝙵𝙸𝙻𝙴</b>
+•> 𝚂𝙴𝙽𝙳 𝙰𝙽𝚈 𝙵𝙸𝙻𝙴 𝙰𝙽𝙳 𝙲𝙻𝙸𝙲𝙺 𝚁𝙴𝙽𝙰𝙼𝙴 𝙾𝙿𝚃𝙸𝙾𝙽 𝙰𝙽𝙳 𝚃𝚈𝙿𝙴 𝙽𝙴𝚆 𝙵𝙸𝙻𝙴 𝙽𝙰𝙼𝙴 𝙰𝙽𝙳 \n 𝚂𝙴𝙽𝙳 𝚂𝙴𝙻𝙴𝙲𝚃 [ 𝙳𝙾𝙲𝚄𝙼𝙴𝙽𝚃, 𝚅𝙸𝙳𝙴𝙾, 𝙰𝚄𝙳𝙸𝙾 ]👈 𝙲𝙷𝙾𝙸𝙲𝙴 𝚃𝙷𝙸𝚂.
+ℹ️ 𝗔𝗻𝘆 𝗢𝘁𝗵𝗲𝗿 𝗛𝗲𝗹𝗽 𝗖𝗼𝗻𝘁𝗮𝗰𝘁 :- <a href=https://t.me/ajay_king_x>♚ ᴀᴊᴀʏ ♚</a>
 """
 
 #⚠️ don't remove our credits 🙏😢😢
     DEV_TXT = """
-<b><u>Special Thanks & Developers</b></u> 
+<b>𝚂𝙿𝙴𝙲𝙸𝙰𝙻 𝚃𝙷𝙰𝙽𝙺𝚂 & 𝙳𝙴𝚅𝙴𝙻𝙾𝙿𝙴𝚁𝚂</b>
 
-» 𝗦𝗢𝗨𝗥𝗖𝗘 𝗖𝗢𝗗𝗘 : <a href=https://github.com/TEAM-PYRO-BOTZ/PYRO-RENAME-BOT>𝐏𝐘𝐑𝐎 𝐑𝐄𝐍𝐀𝐌𝐄 𝐁𝐎𝐓</a>
-» 𝗛𝗢𝗪 𝗧𝗢 𝗗𝗘𝗣𝗟𝗢𝗬 : <a href=https://youtu.be/GfulqsSnTv4>MoTech Yt</a>
+» 𝗦𝗢𝗨𝗥𝗖𝗘 𝗖𝗢𝗗𝗘 : <a href=https://t.me/themastertheblaster>❣️ ∂εcσ∂εɾ รεεɱαɳ ❣️</a>
+» 𝗛𝗢𝗪 𝗧𝗢 𝗗𝗘𝗣𝗟𝗢𝗬 : <a href=https://youtube.com/channel/UCdk53DCXKoKOKgKeohUEnmA>𝗟𝗢𝗢𝗦𝗘𝗥𝗭 𝗖𝗨𝗧𝗭 </a>
 
-• ❣️ <a href=https://github.com/lntechnical2>𝗟𝗡 𝗧𝗘𝗖𝗛𝗡𝗜𝗖</a>
-• ❣️ <a href=https://t.me/Mhd_rzn>𝗠𝗵𝗱_𝗿𝘇𝗻</a>
-• ❣️ <a href=https://youtu.be/GfulqsSnTv4>𝗠𝗼𝗧𝗲𝗰𝗵 𝗬𝗧</a>
-• ❣️ <a href=https://t.me/mr_MKN>𝗠𝗿.𝗠𝗞𝗡 𝗧𝗚</a>
-• ❣️ <a href=https://t.me/GitHub_noob>𝗚𝗶𝘁𝗛𝘂𝗯 𝗡𝗢𝗢𝗕</a>
+• ❣️ <b>𝙾𝚄𝚁 𝙼𝙾𝚂𝚃 𝚂𝙿𝙴𝙲𝙸𝙰𝙻 𝚃𝙷𝙰𝙽𝙺𝚂 𝚃𝙾 </b><a href=https://themastertheblaster>❣️ ∂εcσ∂εɾ ❣️</a></a>\n\n          🙏🏻 𝗧𝗵𝗮𝗻𝗸 𝘆𝗼𝘂 🙏🏻        
 """
